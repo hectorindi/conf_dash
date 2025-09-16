560f70ef080480592f88c2ef63037f97b71ddb63
Piyush Kumar <piyush@tomhorngaming.com>
2025-09-16 13:38:28 +0530

    [conf_dash] Initial commit

---
+++ lib/data/login_service.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+ValueNotifier<AuthService> authService = ValueNotifier(AuthService());
+
+var errorText = "";
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? get currentUser => _auth.currentUser;
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<bool?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return true;
+    } catch (e) {
+      //print('Error signing in: $e');
+      return false;
+    }
+  }
+
+  // bool checkRequiredFields(MemberObject member, String email, String password) {
+  //   if (member.name.isEmpty || member.address == null || member.phoneNumber == null || email.isEmpty || password.isEmpty) {
+  //     errorText = "Please fill all the required fields.";
+  //     return false;
+  //   }
+  //   if (password.length < 6) {
+  //     errorText = "Password must be at least 6 characters long.";
+  //     return false;
+  //   }
+  //   return true;
+  // }
+
+  // Future<LoginObject?> signUp(MemberObject member , String email, String password) async {
+  //   final bool isValid = checkRequiredFields(member, email, password);
+  //   if (!isValid) {
+  //     return LoginObject(name: email, successful: false, uid: null, error: errorText);
+  //   }
+  //   try {
+  //     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+  //       email: email,
+  //       password: password,
+  //     ).then((userCredential) async {
+  //       await memberService.value.addMemberDetailsToDatabase(userCredential, member);
+  //       return userCredential;
+  //     });
+  //     return LoginObject(name: userCredential.user?.displayName ?? '', successful: true, uid: userCredential.user?.uid);
+  //   } catch (e) {
+  //     //print('Error signing up: $e');
+  //     var errorMessage = e.toString().split(  '] ').last; // Extract message after '] '
+  //     return LoginObject(name: email, successful: false, uid: null, error: errorMessage);
+  //   }
+  // }
+
+  Future<bool> signOut() async {
+    try {
+      await _auth.signOut();
+      //print("Sign Out Successful");
+      return true;
+    } catch (e) {
+      //print('Error signing out: $e');
+      return false;
+    }
+    
+  }
+
+  Future<void> updateUserName(String name) async {
+    User? user = currentUser;
+    if (user != null) {
+      await user.updateDisplayName(name);
+    }
+  }
+
+  Future<void> deleteUser() async {
+    User? user = currentUser;
+    if (user != null) {
+      await user.delete();
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:admin/core/constants/color_constants.dart';
+import 'package:admin/core/init/provider_list.dart';
+import 'package:admin/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+final Future<String> _calculation = Future<String>.delayed(
+    const Duration(seconds: 2),
+    () => 'Data Loaded',
+  );
+Widget build(BuildContext context) {
+  return MultiProvider(
+      providers: [...ApplicationProvider.instance.dependItems],
+      child: FutureBuilder(
+        future: _calculation,
+        builder: (context, snapshot) {
+          return MyApp();
+        },
+      ));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Dashboard - Admin Panel v0.1 ',
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(backgroundColor: bgColor, elevation: 0),
+        scaffoldBackgroundColor: bgColor,
+        primaryColor: greenColor,
+        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: Login(title: "Wellcome to the Admin & Dashboard Panel"),
+    );
+  }
+}
